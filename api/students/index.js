a902dea378e13371824084c96f8660a30435d8a3
lauramurf01 <murphy.littltlol.laura@gmil.com>
2017-03-13 10:54:12 +0000

    Test

--- api/students/index.js
+++ api/students/index.js
@@ -1,17 +1,74 @@
-var datastore = require('./datastore');
-var shortId = require('shortid');
-// Get list of contacts
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+var assert = require('assert');
+var config = require('../../config');
+
+var mongoDb;
+
+var url = config.mongodbUri;
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  config.logStars("Connected correctly to server.");
+  mongoDb = db;
+});
+
+// Get list of students
 exports.index = function(req, res) {
-  return res.json(200, datastore.contacts);
-} ;
-// Creates a new contact in datastore.
+   // Connect to the db
+    if (mongoDb){
+      var collection = mongoDb.collection('students');
+      collection.find().toArray(function(err, items) {
+          res.send(items);
+      });
+    }
+    else
+    {
+        config.logStars('No database object!');
+        res.status(404).send({});
+    }
+};
+// Creates a new student in datasbase.
 exports.create = function(req, res) {
-  var contact = {
-     id: shortId.generate(),
-     name: req.body.name,
-     address: req.body.address,
-     phone_number: req.body.phone_number 
-  };
-  datastore.contacts.push(contact)
-  return res.json(201, contact);
+  var student = req.body;
+    if (mongoDb){
+      var collection = mongoDb.collection('students');
+      collection.insertOne(student, function(err, result) {
+            assert.equal(err,null);
+            config.logStars('Inserted: ' + JSON.stringify(result));
+            res.status(200).send(result);
+            
+        });
+    }
+  else
+  {
+    config.logStars('No database object!');
+  }
+   
+};
+
+// Update an existing student in database.
+exports.update = function(req, res) {
+  var id = req.params.id;
+  var student = req.body;
+  config.logStars('Updating student: ' + id);
+  var collection = mongoDb.collection('students');
+  collection.updateOne({'_id':ObjectId(id)}, student, function(err, result) {
+           assert.equal(err,null);
+              console.log('' + result + ' document(s) updated');
+              res.status(200).send(result);
+  });
+   
+};
+// delete an existing student in database.
+exports.delete = function(req, res) {
+    var id = req.params.id;
+  config.logStars('Deleting student: ' + id);
+  var collection = mongoDb.collection('students');
+  collection.deleteOne({'_id':ObjectId(id)}, function(err, result) {
+           assert.equal(err,null);
+          console.log('' + result + ' document(s) deleted');
+          res.status(200).send(result);
+  });
+   
 };
--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var https = require("https");
+
+var key = fs.readFileSync('./ssl/my-server-key.pem');
+var cert = fs.readFileSync('./ssl/my-server-crt.pem');
+var ca = fs.readFileSync('./ssl/my-ca-crt.pem');
+
+var options ={
+
+	key: key,
+	cert: cert,
+	ca:ca
+};
+
+
 //create an express app
 var app = express();
+
+var config = require('./config');
 
 //configure the express app to parse JSON-formatted body
 app.use(bodyParser.json());
@@ -11,13 +28,21 @@ app.get('/',function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("We're up and running!!!");
 });
-// Listen on port 8000, IP defaults to 127.0.0.1
-app.listen(8000)
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
 
 //create routing object
-var student = require('./api/students/index');
-//Add routes forstudents api
-app.get('/api/students',student.index);
-app.post('/api/students',student.create);
+var contact = require('./api/students/index');
+
+//Add routes for contacts api
+app.get('/api/students',contact.index);
+app.post('/api/students',contact.create);
+app.put('/api/students/:id',contact.update);
+app.delete('/api/students/:id',contact.delete);
+
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+var httpsServer = https.Server(options, app)
+  .listen(8000, function () {
+    console.log("Express server running at " + httpsServer.address().port);
+
+  });
+
